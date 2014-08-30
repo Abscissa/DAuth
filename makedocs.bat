508c84c88fab6d1fbaa8540b1cfbde6fb5d38660
Nick Sabalausky <Abscissa>
2014-08-30 04:19:05 -0400

    docs: Omit privates from docs.

--- makedocs.bat
+++ makedocs.bat
@@ -6,4 +6,5 @@ rem https://github.com/rejectedsoftware/ddox
 rdmd -Isrc --build-only --force -c -Dddocs_tmp -X -Xfdocs\docs.json src\dauth\package.d
 rmdir /S /Q docs_tmp > NUL 2> NUL
 del src\dauth\package.exe
-ddox generate-html docs\docs.json docs\public
+ddox filter docs\docs.json --min-protection=Protected
+ddox generate-html docs\docs.json docs\public --navigation-type=ModuleTree
