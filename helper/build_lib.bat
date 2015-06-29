03db789c1f93c750ea72795b000f35d8e76a13ea
Nick Sabalausky <Abscissa@users.noreply.github.com>
2015-06-28 20:16:04 -0400

    Get rid of junk files that get generated when building library.

--- helper/build_lib.bat
+++ helper/build_lib.bat
@@ -11,3 +11,6 @@ dub build
 rem Compile
 cd /D %2
 %3bin\dub-data-mod describe -q --compiler=dmd --config=library --data-0 --data=options --data=versions --data=import-paths | %4bin\safearg --post=src\instauser\%1\package.d rdmd --build-only -lib -od. -of%2lib\instauser-%1.lib --force
+
+rem Remove junk that got generated
+del package.lib > NUL 2> NUL
