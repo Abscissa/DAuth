04563db9fe83bbff500ad480c38c659115d461d2
Nick Sabalausky <Abscissa@users.noreply.github.com>
2015-08-18 18:26:59 -0400

    Convert 'helper/build_tests' from shell to D.

--- helper/build_tests.bat
+++ helper/build_tests.bat
@@ -1,10 +1,2 @@
 @echo off
-
-rem Ensure safearg is built
-cd /D %3
-dub build
-
-rem Compile
-echo Compiling/Running instauser-%1 tests...
-cd /D %2
-dub describe --nodeps --compiler=dmd --config=tests --data-0 --data=options,versions,import-paths,linker-files | %3bin\safearg --post=src\instauser\%1\package.d rdmd -Ires -Jres -ofbin\instauser-%1-unittests -debug -g -unittest -main --force
+rdmd ..\helper\build_tests.d %*
---
+++ helper/build_tests.d
@@ -0,0 +1,60 @@
+import std.algorithm;
+import std.exception;
+import std.file;
+import std.path;
+import std.process;
+import std.range;
+import std.stdio;
+
+int main(string[] args)
+{
+	// Check args
+	if(args.length != 6)
+	{
+		stderr.writeln(
+			"Usage:"
+			" rdmd ../helper/build_tests.d (basic|store|web) $PACKAGE_DIR",
+			" $SAFEARG_PACKAGE_DIR $DDOX_PACKAGE_DIR $ROOT_PACKAGE_DIR"
+		);
+		return 1;
+	}
+	
+	// Get args
+	const subProjectName = args[1];
+	const packageDir     = args[2];
+	const safeArgDir     = args[3];
+	const ddoxDir        = args[4];
+	const rootPackageDir = args[5];
+	
+	// Save current working dir
+	immutable origWorkingDir = getcwd();
+	scope(exit) chdir(origWorkingDir);
+	
+	// Ensure safearg is built
+	chdir(safeArgDir);
+	spawnShell("dub build").wait();
+	
+	// Build commands
+	version(Posix) enum extraArgs = " -L-levent_pthreads -L-levent -L-lssl -L-lcrypto";
+	else           enum extraArgs = "";
+
+	const safeArgTool = escapeShellFileName( buildNormalizedPath(safeArgDir, "bin/safearg") );
+
+	const dubDescribeDataCmd =
+		"dub describe --nodeps --compiler=dmd --config=tests --data-0 "~
+		"--data=options,versions,import-paths,linker-files";
+
+	const rdmdCmd =
+		"rdmd -Ires -Jres -ofbin/instauser-"~subProjectName~"-unittest "~
+		"-debug -g -unittest -main --force"~extraArgs;
+
+	const safeArgToRdmdCmd =
+		safeArgTool~" --post=src/instauser/"~subProjectName~"/package.d "~rdmdCmd;
+	
+	// Compile/Run tests
+	writeln("Compiling/Running instauser-", subProjectName, " tests...");
+	chdir(packageDir);
+	spawnShell(dubDescribeDataCmd~" | "~safeArgToRdmdCmd).wait();
+	
+	return 0;
+}
