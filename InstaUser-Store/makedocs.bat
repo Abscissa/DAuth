f3ddc0d3bf4511fee3ca2cc0e85afd1d6ec38b83
Nick Sabalausky <Abscissa@users.noreply.github.com>
2015-08-19 13:25:31 -0400

    Consistent naming/content for user-facing build scripts.

--- InstaUser-Store/makedocs.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-dmd -Isrc -c -op -Dddocs -version=Have_mysql_native -version=Have_vibe_d %* src\instauser\store\package.d src\instauser\core.d src\instauser\store\memory.d src\instauser\store\mysqln.d
-del docs\src\instauser\store\index.html > NUL 2> NUL
-rename docs\src\instauser\store\package.html index.html
---
+++ InstaUser-Web/run_tests.bat
@@ -0,0 +1,2 @@
+@echo off
+dub build --force --config=tests
