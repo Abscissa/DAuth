aa45c0d1be1dc1c23ad37cbfece94104702dfc83
Nick Sabalausky <Abscissa@users.noreply.github.com>
2015-08-19 14:15:58 -0400

    Use --nodeps when building InstaUser-Web's tests.

--- InstaUser-Web/run_tests.bat
+++ InstaUser-Web/run_tests.bat
@@ -1,2 +1,8 @@
 @echo off
-dub build --force --config=tests
+echo Note: If this fails complaining about unresolved dependencies, run 
+echo dub build --vverbose
+echo and then ctrl-c when it gets to a loop to seems to take a very long time.
+echo "(See <https://github.com/D-Programming-Language/dub/issues/652>)"
+echo Then try running run_tests again.
+
+dub build --force --nodeps --config=tests
