c877ee3c09817870643d9432503929ea27dfb5e9
Nick Sabalausky <Abscissa>
2017-01-31 01:48:56 -0500

    Regression test issue #4

--- src/dauth/package.d
+++ src/dauth/package.d
@@ -51,4 +51,26 @@ version(DAuth_Unittest)
 {
 	import dauth.sha;
 	import dauth.hashdrbg;
+
+	unittest
+	{
+		import std.process;
+
+		unitlog("Testing different results on different executions");
+		assert(
+			spawnShell(`rdmd --build-only --force -Isrc -ofbin/genBytes genBytes.d`).wait()
+			== 0, "Failed to compile genBytes.d"
+		);
+		enum cmd = "bin/genBytes";
+		auto result1 = execute(cmd);
+		auto result2 = execute(cmd);
+		auto result3 = execute(cmd);
+		assert(result1.status == 0, "Command failed: "~cmd);
+		assert(result2.status == 0, "Command failed: "~cmd);
+		assert(result3.status == 0, "Command failed: "~cmd);
+
+		assert(result1.output != result2.output);
+		assert(result2.output != result3.output);
+		assert(result3.output != result1.output);
+	}
 }
